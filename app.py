6ebcf7c3630c67e40b2c5991605c0d2b8ca50aaf
MohitRathi14 <aaditya375singh2@gmail.com>
2026-02-24 13:29:33 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
 from fastapi import FastAPI, UploadFile, File
+from pydantic import BaseModel
 import shutil
 import os
+import requests
 from predict import predict_image
 
-app = FastAPI()
+app = FastAPI(title="Sugarcane Disease Detection API")
 
 @app.get("/")
-def home():
-    return {"message": "API running"}
+def health():
+    return {"message": "ML Service Running"}
 
-@app.post("/predict")
-async def predict(file: UploadFile = File(...)):
 
-    temp_path = f"temp_{file.filename}"
+class ImageURL(BaseModel):
+    image_url: str
+
+@app.post("/predict-url")
+async def predict_url(data: ImageURL):
+
+    temp_path = "temp.jpg"
 
     try:
-        with open(temp_path, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
+        response = requests.get(data.image_url)
+
+        with open(temp_path, "wb") as f:
+            f.write(response.content)
 
         result = predict_image(temp_path)
 
@@ -25,3 +33,19 @@ async def predict(file: UploadFile = File(...)):
     finally:
         if os.path.exists(temp_path):
             os.remove(temp_path)
+# @app.post("/predict")
+# async def predict(file: UploadFile = File(...)):
+
+#     temp_path = f"temp_{file.filename}"
+
+#     try:
+#         with open(temp_path, "wb") as buffer:
+#             shutil.copyfileobj(file.file, buffer)
+
+#         result = predict_image(temp_path)
+
+#         return result
+
+#     finally:
+#         if os.path.exists(temp_path):
+#             os.remove(temp_path)
